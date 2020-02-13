9a40a761c8e2778513774523a89e7ce65d4b5edd
RaJan <60703617+rajan-03@users.noreply.github.com>
2020-02-13 16:03:33 +0545

    Create noargument.java

---
+++ lab02/noargument.java
@@ -0,0 +1,10 @@
+class Demo
+{
+     public Demo()
+     {
+         System.out.println("This is a no argument constructor");
+     }
+     public static void main(String args[]) {
+    	 new Demo();
+     }
+}
