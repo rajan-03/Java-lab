576c4bdf3c498bcd274bd372175cc68e1633b4ca
RaJan <60703617+rajan-03@users.noreply.github.com>
2020-02-13 15:18:33 +0545

    Update lab.java

--- lab 01/lab.java
+++ lab 01/lab.java
@@ -42,7 +42,7 @@ a[1]=20;
 a[2]=70;  
 a[3]=40;  
 a[4]=50;   
-for(int i=0;i<a.length;i++)//length is the property of array  
+for(int i=0;i<a.length;i++) 
 System.out.println(a[i]);  
 }
 }
@@ -63,7 +63,7 @@ public static void main(String args[]){
 int arr[][]={{1,2,3},{2,4,5},{4,4,5}};  
 for(int i=0;i<3;i++){  
  for(int j=0;j<3;j++){  
-   System.out.print(arr[i][j]+" ");  
+ System.out.print(arr[i][j]+" ");  
  }  
  System.out.println();  
 }
