9bc5351a2e0eb7453325cf6f9a157a2dc0d390ea
RaJan <60703617+rajan-03@users.noreply.github.com>
2020-02-19 15:07:40 +0545

    Create package.java

---
+++ lab04/package.java
@@ -0,0 +1,35 @@
+package MyPackage;
+ 
+public class Compare {
+  int num1, num2;
+ 
+  Compare(int n, int m) {
+     num1 = n;
+     num2 = m;
+}
+public void getmax(){
+    if ( num1 > num2 ) {
+        System.out.println("Maximum value of two numbers is " + num1);
+  }
+    else {
+        System.out.println("Maximum value of two numbers is " + num2);
+    }
+}
+ 
+ 
+public static void main(String args[]) {
+        Compare current[] = new Compare[3];
+            
+        current[1] = new Compare(5, 10);
+        current[2] = new Compare(123, 120);
+          
+          for(int i=1; i < 3 ; i++)
+              {
+              current[i].getmax();
+              }
+       }
+}
+
+output
+Maximum value of two numbers is 10
+Maximum value of two numbers is 123
